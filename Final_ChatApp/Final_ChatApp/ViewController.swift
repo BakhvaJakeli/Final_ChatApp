ceff9e6c48879ced195f70d5b526016c4644301a
BakhvaJakeli <85591231+BakhvaJakeli@users.noreply.github.com>
2021-07-23 16:06:50 +0400

    Add files via upload

---
+++ Final_ChatApp/Final_ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final_ChatApp
+//
+//  Created by Baxva Jakeli on 23.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
